5d311f3bb7f9824fffaebee4ee9c6bc27e2baf74
nobiart <dmitri.besleaga@gmail.com>
2024-07-07 00:01:34 +0300

    refactor: clean the tech code

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,20 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Hello!</h1>
+      <h3>I hope you're doing well.</h3>
+      <h4>
+        Could you please check my assignment on Monday (July 8th)? I wasn't able
+        to finish everything on time.
+      </h4>
+      <br />
+      <h3>Thank you so much for your understanding!</h3>
+      <br />
+      <p>P.S.</p>
+      <p>My Name is Dmitri Besleaga (@nobiart).</p>
+      <p>Please DM me if you have any questions.</p>
     </>
   );
 }
