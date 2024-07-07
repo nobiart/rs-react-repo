63bbf184608b0e895e4691c0610255b9173aa999
nobiart <dmitri.besleaga@gmail.com>
2024-07-08 01:35:19 +0300

    refactor: remove lifecycle methods from the "Content"

--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -1,21 +1,17 @@
 import React from "react";
 import s from "./content.module.css";
 import { Card } from "../card/Card.tsx";
-import { getPersons, IPerson } from "../../api/persons.ts";
+import { IPerson } from "../../api/persons.ts";
 
-export class Content extends React.Component {
-  state = {
-    persons: [],
-  };
+interface IProps {
+  persons: IPerson[];
+}
 
-  componentDidMount() {
-    getPersons().then((res) => this.setState({ persons: res.results }));
-  }
-
+export class Content extends React.Component<IProps> {
   render() {
     return (
       <div className={s.content}>
-        {this.state.persons.map((person: IPerson) => (
+        {this.props.persons.map((person: IPerson) => (
           <Card key={person.name} person={person} />
         ))}
       </div>
